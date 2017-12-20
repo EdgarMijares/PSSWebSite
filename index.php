4433d3e6264a96665b075f4da52fe82ebcadbea8
Edgar <progresssystemsoftware@gmail.com>
2017-12-20 11:06:31 -0600

    Crear proyecto

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+	<meta charset="UTF-8">
+	<title>Progress System Software</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/fontawesome/font-awesome.min.css">
+	<link rel="stylesheet" href="css/fullpage/jquery.fullpage.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Barlow:300,400,600" rel="stylesheet">
+	<link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+	<header>
+		<?php include("menu.html") ?>
+	</header>
+	<section class="main container" id="fullpage">
+		<div class="row textos section">
+			<div class="col-md-12">
+				<h2 class="primera_linea">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+				<h3 class="segunda_linea">Quo ratione blanditiis eum, facere distinctio quod</h3>
+				<div class="contenedor-btns">
+					<a href="#">Caracteristicas</a>
+					<a href="#">Comprar ahora</a>
+				</div>
+			</div>
+		</div>
+		<div class="caracteristicas section">
+			<div class="row">
+				<div class="col-md-12">
+					<h3>Caracteristicas</h3>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-4 caracteristica">
+					<img src="img/iconos/icono1.png" alt="">
+					<h3 class="primera_linea">Lorem ipsum.</h3>
+					<p class="parrafo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, porro.</p>
+				</div>
+
+				<div class="col-sm-4 caracteristica">
+					<img src="img/iconos/icono2.png" alt="">
+					<h3 class="primera_linea">Lorem ipsum dolor.</h3>
+					<p class="parrafo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. .</p>
+				</div>
+
+				<div class="col-sm-4 caracteristica">
+					<img src="img/iconos/icono4.png" alt="">
+					<h3 class="primera_linea">Lorem ipsum.</h3>
+					<p class="parrafo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea neque pariatur quidem reprehenderit amet cupiditate.</p>
+				</div>
+			</div>
+		</div>
+		<div class="fotos section"></div>
+		<div class="clientes section"></div>
+		<div class="precios section"></div>
+		<div class="review section"></div>
+	</section>
+	<footer>
+		
+	</footer>
+
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script type="text/javascript" src="js/fullpage/jquery.fullpage.min.js"></script>
+	<script type="text/javascript" src="js/fullpage/vendors/jquery.easings.min.js"></script>
+	<script type="text/javascript" src="js/fullpage/vendors/scrolloverflow.min.js"></script>
+	<script type="text/javascript" src="js/main.min.js"></script>
+</body>
+</html>
