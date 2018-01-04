285cde7fc9ac583b2bec5320f9ef0f68c04d650b
Edgar <progresssystemsoftware@gmail.com>
2018-01-04 01:18:33 -0600

    Primeros estilos

--- index.php
+++ index.php
@@ -12,23 +12,27 @@
 </head>
 <body>
 	<header>
-		<?php include("menu.html") ?>
-	</header>
-	<section class="main container" id="fullpage">
-		<div class="row textos section">
-			<div class="col-md-12">
-				<h2 class="primera_linea">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
-				<h3 class="segunda_linea">Quo ratione blanditiis eum, facere distinctio quod</h3>
-				<div class="contenedor-btns">
-					<a href="#">Caracteristicas</a>
-					<a href="#">Comprar ahora</a>
+		<div class="filtro">
+			<?php include("menu.html") ?>
+			<div class="row textos">
+				<div class="col-md-12">
+					<img src="img/logosolo.png" alt="" class="logo">
+					<h2 class="primera_linea">PROGRESS SYSTEM SOFTWARE</h2>
+					<h3 class="segunda_linea">EL PROGRESO SIEMPRE IMPLICARA UN RIESGO</h3>
+					<div class="contenedor-btns">
+						<a href="#">Caracteristicas</a>
+						<a href="#">Costos $</a>
+					</div>
 				</div>
 			</div>
+			
 		</div>
-		<div class="caracteristicas section">
+	</header>
+	<section class="main container" id="fullpage">
+		<div class="caracteristicas">
 			<div class="row">
 				<div class="col-md-12">
-					<h3>Caracteristicas</h3>
+					<h3 class="titulo">Caracteristicas</h3>
 				</div>
 			</div>
 			<div class="row">
@@ -51,19 +55,171 @@
 				</div>
 			</div>
 		</div>
-		<div class="fotos section"></div>
-		<div class="clientes section"></div>
-		<div class="precios section"></div>
-		<div class="review section"></div>
+
+		<div class="fotos">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="row foto">
+							<div class="col-sm-4 thumb">
+								<img class="img-fluid" src="img/imagen1.png" alt="">
+							</div>
+							<div class="col-sm-8 texto">
+								<h2 class="titulo azul">Lorem ipsum dolor.</h2>
+								<p class="parrafo">
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora repellat vitae voluptatem placeat iusto nemo architecto error, maiores nostrum a vel perferendis temporibus facilis, dolor possimus fugit consectetur doloribus quibusdam.
+								</p>
+							</div>
+						</div>
+
+						<div class="row foto">
+							<div class="col-sm-8 texto">
+								<h2 class="titulo naranja derecha">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+								<p class="parrafo derecha">
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam quia, perspiciatis dolore illum neque doloremque voluptatibus tenetur officia reiciendis totam architecto eos porro molestias optio quis nobis iure ab numquam?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem laborum odio nostrum, possimus
+									
+								</p>
+							</div>
+							<div class="col-sm-4 thumb">
+								<img class="img-fluid" src="img/imagen1.jpg" alt="">
+							</div>
+						</div>
+
+						<div class="row foto">
+							<div class="col-sm-4 thumb">
+								<img class="img-fluid" src="img/imagen1.jpg" alt="">
+							</div>
+							<div class="col-sm-8 texto">
+								<h2 class="titulo azul">Lorem ipsum dolor sit amet.</h2>
+								<p class="parrafo">
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora repellat vitae voluptatem placeat iusto nemo architecto error, maiores nostrum a vel perferendis temporibus facilis, dolor possimus fugit consectetur doloribus quibusdam.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="clientes">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h3 class="titulo">Clientes recientes</h3>
+					</div>
+				</div>
+				<div class="row">
+					<div class="cliente col-xs-6 col-md-3">
+						<a href="#"><img class="" src="img/clientes/innovatio.png" alt=""></a>
+					</div>
+					<div class="cliente col-xs-6 col-md-3">
+						<a href="#"><img class="" src="img/clientes/pancholines.png" alt=""></a>
+					</div>
+					<div class="cliente col-xs-6 col-md-3">
+						<a href="#"><img class="" src="img/clientes/whitesmile.png" alt=""></a>
+					</div>
+					<div class="cliente col-xs-6 col-md-3">
+						<a href="#"><img class="" src="img/clientes/seandse.png" alt=""></a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="precios">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h3>Precios</h3>
+					</div>
+				</div>
+				<div class="row">
+					<div class="precio col-sm-4">
+						<div class="plan color1">
+							<p class="nombre">Sitio web</p>
+							<p class="precio">$3500</p> 
+							<ul class="caracteristicas">
+								<li>1 cuenta</li>
+								<li>10 GB de espacio</li>
+								<li>Correo electronico</li>
+							</ul>
+							<a href="#" class="btn">Mas información</a>
+						</div>
+					</div>
+					<div class="precio col-sm-4">
+						<div class="plan color2">
+							<p class="nombre">Aplicacion web</p>
+							<p class="precio">$5500</p> 
+							<ul class="caracteristicas">
+								<li>3 cuentas</li>
+								<li>10 GB de espacio</li>
+								<li>Base de datos</li>
+							</ul>
+							<a href="#" class="btn">Mas información</a>
+						</div>
+					</div>
+					<div class="precio col-sm-4">
+						<div class="plan color3">
+							<p class="nombre">Software</p>
+							<p class="precio">$ - - - -</p> 
+							<ul class="caracteristicas">
+								<li>Documentación</li>
+								<li>Desarrollo</li>
+								<li>Implementacón</li>
+							</ul>
+							<a href="#" class="btn">Mas información</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</section>
 	<footer>
-		
+		<div class="contacto">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8">
+						<h3>Contacto</h3>
+						<div class="row">
+							<div class="col-md-12">
+								<button type="button" class="btn btn-primary">Formulario</button>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-4">
+								<a href="#" class="facebook"><span class="fa fa-facebook-official"></span></a>
+							</div>
+							<div class="col-4">
+								<a href="#" class="linkedin"><span class="fa fa-linkedin"></span></a>
+							</div>
+							<div class="col-4">
+								<a href="#" class="instagram"><span class="fa fa-instagram"></span></a>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-6">
+								<a href="tel:016181859879" class=""><span class="fa fa-phone-square"></span> 618-185-98-79</a>
+							</div>
+							<div class="col-md-6">
+								<a href="tel:016181571896" class=""><span class="fa fa-phone-square"></span> 618-157-18-96</a>
+							</div>
+							<div class="col-md-12">
+								<a href="mailto:contacto@pssoftware.com.mx"><span class="fa fa-commenting-o"></span> CONTACTO@PSSOFTWARE.COM.MX</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<img src="img/logosolo.png" alt="" style="align-content: right;">
+					</div>
+				</div>
+			</div>
+		</div>
 	</footer>
 
 	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-	<script type="text/javascript" src="js/fullpage/jquery.fullpage.min.js"></script>
+	<!--script type="text/javascript" src="js/fullpage/jquery.fullpage.min.js"></script>
 	<script type="text/javascript" src="js/fullpage/vendors/jquery.easings.min.js"></script>
-	<script type="text/javascript" src="js/fullpage/vendors/scrolloverflow.min.js"></script>
+	<script type="text/javascript" src="js/fullpage/vendors/scrolloverflow.min.js"></script-->
+	
 	<script type="text/javascript" src="js/main.min.js"></script>
 </body>
 </html>
