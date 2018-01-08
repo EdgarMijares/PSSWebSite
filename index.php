04559ead3b638d618b730e174c9d85ebaf83e50c
Edgar <progresssystemsoftware@gmail.com>
2018-01-08 15:23:46 -0600

    Imagenes nuevas

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
 	<link rel="stylesheet" href="css/fullpage/jquery.fullpage.min.css">
 	<link href="https://fonts.googleapis.com/css?family=Barlow:300,400,600" rel="stylesheet">
 	<link rel="stylesheet" href="css/estilos.css">
+
+	<meta name="theme-color" content="#26424B">
 </head>
 <body>
 	<header>
@@ -16,7 +18,7 @@
 			<?php include("menu.html") ?>
 			<div class="row textos">
 				<div class="col-md-12">
-					<img src="img/logosolo.png" alt="" class="logo">
+					<img src="img/logosolo.png" alt="Progress System Software logo" class="logo">
 					<h2 class="primera_linea">PROGRESS SYSTEM SOFTWARE</h2>
 					<h3 class="segunda_linea">EL PROGRESO SIEMPRE IMPLICARA UN RIESGO</h3>
 					<div class="contenedor-btns">
@@ -38,20 +40,20 @@
 			<div class="row">
 				<div class="col-sm-4 caracteristica">
 					<img src="img/iconos/icono1.png" alt="">
-					<h3 class="primera_linea">Lorem ipsum.</h3>
-					<p class="parrafo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, porro.</p>
+					<h3 class="primera_linea">Analítica</h3>
+					<p class="parrafo">A través de la analítica web podremos analizar con mucho más detalle los resultados de las acciones de marketing y tomar las decisiones más  acertadas sobre nuestra web.</p>
 				</div>
 
 				<div class="col-sm-4 caracteristica">
 					<img src="img/iconos/icono2.png" alt="">
-					<h3 class="primera_linea">Lorem ipsum dolor.</h3>
-					<p class="parrafo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. .</p>
+					<h3 class="primera_linea">Sitio web</h3>
+					<p class="parrafo">Con un sitio Web, potenciales clientes pueden informarse de sus productos y servicios a cualquier hora, desde cualquier lugar con conexión a Internet</p>
 				</div>
 
 				<div class="col-sm-4 caracteristica">
 					<img src="img/iconos/icono4.png" alt="">
-					<h3 class="primera_linea">Lorem ipsum.</h3>
-					<p class="parrafo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea neque pariatur quidem reprehenderit amet cupiditate.</p>
+					<h3 class="primera_linea">Software</h3>
+					<p class="parrafo">Mejoran la capacidad y gestión operativa tanto interna como externa de las compañías y el nivel de competitividad de los negocios se mantiene a la altura del mercado.</p>
 				</div>
 			</div>
 		</div>
@@ -81,13 +83,13 @@
 								</p>
 							</div>
 							<div class="col-sm-4 thumb">
-								<img class="img-fluid" src="img/imagen1.jpg" alt="">
+								<img class="img-fluid" src="img/imagen2.png" alt="">
 							</div>
 						</div>
 
 						<div class="row foto">
 							<div class="col-sm-4 thumb">
-								<img class="img-fluid" src="img/imagen1.jpg" alt="">
+								<img class="img-fluid" src="img/imagen3.png" alt="">
 							</div>
 							<div class="col-sm-8 texto">
 								<h2 class="titulo azul">Lorem ipsum dolor sit amet.</h2>
@@ -110,16 +112,16 @@
 				</div>
 				<div class="row">
 					<div class="cliente col-xs-6 col-md-3">
-						<a href="#"><img class="" src="img/clientes/innovatio.png" alt=""></a>
+						<a target="_blank" href="http://dentalws.com/"><img class="" src="img/clientes/whitesmile.png" alt=""></a>
 					</div>
 					<div class="cliente col-xs-6 col-md-3">
-						<a href="#"><img class="" src="img/clientes/pancholines.png" alt=""></a>
+						<a target="_blank" href="http://seandse.com/"><img class="" src="img/clientes/seandse.png" alt=""></a>
 					</div>
 					<div class="cliente col-xs-6 col-md-3">
-						<a href="#"><img class="" src="img/clientes/whitesmile.png" alt=""></a>
+						<a target="_blank" href="http://www.pancholines.com.mx/"><img class="" src="img/clientes/pancholines.png" alt=""></a>
 					</div>
 					<div class="cliente col-xs-6 col-md-3">
-						<a href="#"><img class="" src="img/clientes/seandse.png" alt=""></a>
+						<a target="_blank" href="https://www.facebook.com/HackerSpaceDGO/"><img class="" src="img/clientes/innovatio.png" alt=""></a>
 					</div>
 				</div>
 			</div>
