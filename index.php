085df4b1bca137a9a041eb62da3c58cc02ca2571
Edgar <progresssystemsoftware@gmail.com>
2018-01-15 17:04:53 -0600

    Optmizar bootstrap

--- index.php
+++ index.php
@@ -30,30 +30,32 @@
 			
 		</div>
 	</header>
-	<section class="main container" id="fullpage">
+	<section class="main" id="fullpage">
 		<div class="caracteristicas">
-			<div class="row">
-				<div class="col-md-12">
-					<h3 class="titulo">Caracteristicas</h3>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h3 class="titulo">Caracteristicas</h3>
+					</div>
 				</div>
-			</div>
-			<div class="row">
-				<div class="col-sm-4 caracteristica">
-					<img src="img/iconos/icono1.png" alt="">
-					<h3 class="primera_linea">Analítica</h3>
-					<p class="parrafo">A través de la analítica web podremos analizar con mucho más detalle los resultados de las acciones de marketing y tomar las decisiones más  acertadas sobre nuestra web.</p>
-				</div>
-
-				<div class="col-sm-4 caracteristica">
-					<img src="img/iconos/icono2.png" alt="">
-					<h3 class="primera_linea">Sitio web</h3>
-					<p class="parrafo">Con un sitio Web, potenciales clientes pueden informarse de sus productos y servicios a cualquier hora, desde cualquier lugar con conexión a Internet</p>
-				</div>
-
-				<div class="col-sm-4 caracteristica">
-					<img src="img/iconos/icono4.png" alt="">
-					<h3 class="primera_linea">Software</h3>
-					<p class="parrafo">Mejoran la capacidad y gestión operativa tanto interna como externa de las compañías y el nivel de competitividad de los negocios se mantiene a la altura del mercado.</p>
+				<div class="row">
+					<div class="col-sm-4 caracteristica">
+						<img src="img/iconos/icono1.png" alt="">
+						<h3 class="primera_linea">Analítica</h3>
+						<p class="parrafo">A través de la analítica web podremos analizar con mucho más detalle los resultados de las acciones de marketing y tomar las decisiones más  acertadas sobre nuestra web.</p>
+					</div>
+				
+					<div class="col-sm-4 caracteristica">
+						<img src="img/iconos/icono2.png" alt="">
+						<h3 class="primera_linea">Sitio web</h3>
+						<p class="parrafo">Con un sitio Web, potenciales clientes pueden informarse de sus productos y servicios a cualquier hora, desde cualquier lugar con conexión a Internet</p>
+					</div>
+				
+					<div class="col-sm-4 caracteristica">
+						<img src="img/iconos/icono4.png" alt="">
+						<h3 class="primera_linea">Software</h3>
+						<p class="parrafo">Mejoran la capacidad y gestión operativa tanto interna como externa de las compañías y el nivel de competitividad de los negocios se mantiene a la altura del mercado.</p>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -64,37 +66,50 @@
 					<div class="col-md-12">
 						<div class="row foto">
 							<div class="col-sm-4 thumb">
-								<img class="img-fluid" src="img/imagen1.png" alt="">
+								<img class="img-fluid posicion-img-0" src="img/imagen3.png" alt="">
 							</div>
-							<div class="col-sm-8 texto">
-								<h2 class="titulo azul">Lorem ipsum dolor.</h2>
+							<div class="col-sm-8 texto-0">
+								<h2 class="titulo azul">Analitica digital</h2>
 								<p class="parrafo">
-									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora repellat vitae voluptatem placeat iusto nemo architecto error, maiores nostrum a vel perferendis temporibus facilis, dolor possimus fugit consectetur doloribus quibusdam.
+									Midiendo el comportamiento de tus clientes ves lo que funciona y lo que no funciona de tu estrategia digital, y de esta forma tomas decisiones basadas en datos para:
+									<ul class="lista">
+										<li>Aumentar el rendimiento de la inversión en marketing online.</li>
+										<li>Mejorar la experiencia de usuario.</li>
+										<li>Lograr los resultados deseados.</li>
+									</ul>
+									<span class="delete_responsive">La analítica digital abarca analítica web, analítica social y analítica móvil. Tiene en cuenta todos los puntos de contacto online entre empresa y usuario o cliente para que conozcas el ROI de todas las acciones de marketing digital y así poder seguir la mejor estrategia de medios en cada momento.</span>
 								</p>
 							</div>
 						</div>
+						<hr>
 
-						<div class="row foto">
-							<div class="col-sm-8 texto">
-								<h2 class="titulo naranja derecha">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+						<div class="row foto orden">
+							<div class="col-sm-8 texto-1">
+								<h2 class="titulo naranja derecha">Sitio web personalizado</h2>
 								<p class="parrafo derecha">
-									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam quia, perspiciatis dolore illum neque doloremque voluptatibus tenetur officia reiciendis totam architecto eos porro molestias optio quis nobis iure ab numquam?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem laborum odio nostrum, possimus
-									
+									Todos nuestros sitios web son 100% únicos sin plantillas, para que refleje tu marca de la mejor manera. El servicio puede variar dependiendo de tus necesidades, y nosotros estamos para asesorarte de la mejor manera posible, para que tu sitio cumpla su objetivo captar clientes.
 								</p>
 							</div>
 							<div class="col-sm-4 thumb">
-								<img class="img-fluid" src="img/imagen2.png" alt="">
+								<img class="img-fluid posicion-img-1" src="img/imagen1.png" alt="">
 							</div>
 						</div>
+						<hr>
 
 						<div class="row foto">
 							<div class="col-sm-4 thumb">
-								<img class="img-fluid" src="img/imagen3.png" alt="">
+								<img class="img-fluid posicion-img-2" src="img/imagen2.png" alt="">
 							</div>
-							<div class="col-sm-8 texto">
-								<h2 class="titulo azul">Lorem ipsum dolor sit amet.</h2>
+							<div class="col-sm-8 texto-2">
+								<h2 class="titulo azul">Desarrollo de software</h2>
 								<p class="parrafo">
-									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora repellat vitae voluptatem placeat iusto nemo architecto error, maiores nostrum a vel perferendis temporibus facilis, dolor possimus fugit consectetur doloribus quibusdam.
+									Un software empresarial, por ejemplo, ofrece multitud de beneficios, entre los que destacan los siguientes:
+									<ul class="lista">
+										<li>Automatización del proceso administrativo y burocrático</li>
+										<li>Infraestructura necesaria para el control de la gestión</li>
+										<li>Pieza clave en el diseño de la organización y de sus actividades</li>
+										<li>Parte integrante del servicio ofrecido por la compañía</li>
+									</ul>
 								</p>
 							</div>
 						</div>
@@ -102,7 +117,7 @@
 				</div>
 			</div>
 		</div>
-
+		
 		<div class="clientes">
 			<div class="container">
 				<div class="row">
@@ -111,17 +126,23 @@
 					</div>
 				</div>
 				<div class="row">
-					<div class="cliente col-xs-6 col-md-3">
-						<a target="_blank" href="http://dentalws.com/"><img class="" src="img/clientes/whitesmile.png" alt=""></a>
+					<div class="cliente col-0 col-md-2">
+						<a target="_blank" href=""><img class="img-fluid" src="" alt=""></a>
 					</div>
-					<div class="cliente col-xs-6 col-md-3">
-						<a target="_blank" href="http://seandse.com/"><img class="" src="img/clientes/seandse.png" alt=""></a>
+					<div class="cliente col-3 col-md-2">
+						<a target="_blank" href="http://dentalws.com/"><img class="img-fluid" src="img/clientes/whitesmile.png" alt="Clinica dental White Smile" width="100%"></a>
 					</div>
-					<div class="cliente col-xs-6 col-md-3">
-						<a target="_blank" href="http://www.pancholines.com.mx/"><img class="" src="img/clientes/pancholines.png" alt=""></a>
+					<div class="cliente col-3 col-md-2">
+						<a target="_blank" href="http://seandse.com/"><img class="img-fluid" src="img/clientes/seandse.png" alt="Seguridad y servicio SEANDSE"></a>
 					</div>
-					<div class="cliente col-xs-6 col-md-3">
-						<a target="_blank" href="https://www.facebook.com/HackerSpaceDGO/"><img class="" src="img/clientes/innovatio.png" alt=""></a>
+					<div class="cliente col-3 col-md-2">
+						<a target="_blank" href="http://www.pancholines.com.mx/"><img class="img-fluid" src="img/clientes/pancholines.png" alt="Uniformes pancholines"></a>
+					</div>
+					<div class="cliente col-3 col-md-2">
+						<a target="_blank" href="https://www.facebook.com/HackerSpaceDGO/"><img class="img-fluid" src="img/clientes/innovatio.png" alt="Innovation Room Hacker Space DGO"></a>
+					</div>
+					<div class="cliente col-0 col-md-2">
+						<a target="_blank" href=""><img class="img-fluid" src="" alt=""></a>
 					</div>
 				</div>
 			</div>
@@ -135,7 +156,7 @@
 					</div>
 				</div>
 				<div class="row">
-					<div class="precio col-sm-4">
+					<div class="precio col-md-4 col-12">
 						<div class="plan color1">
 							<p class="nombre">Sitio web</p>
 							<p class="precio">$3500</p> 
@@ -144,10 +165,11 @@
 								<li>10 GB de espacio</li>
 								<li>Correo electronico</li>
 							</ul>
+							<p class="nota">Paquete basico no incluye mejoras.</p>
 							<a href="#" class="btn">Mas información</a>
 						</div>
 					</div>
-					<div class="precio col-sm-4">
+					<div class="precio col-md-4 col-12">
 						<div class="plan color2">
 							<p class="nombre">Aplicacion web</p>
 							<p class="precio">$5500</p> 
@@ -156,10 +178,11 @@
 								<li>10 GB de espacio</li>
 								<li>Base de datos</li>
 							</ul>
+							<p class="nota">No incluye el costo de la aplicaión.</p>
 							<a href="#" class="btn">Mas información</a>
 						</div>
 					</div>
-					<div class="precio col-sm-4">
+					<div class="precio col-md-4 col-12">
 						<div class="plan color3">
 							<p class="nombre">Software</p>
 							<p class="precio">$ - - - -</p> 
@@ -168,6 +191,7 @@
 								<li>Desarrollo</li>
 								<li>Implementacón</li>
 							</ul>
+							<p class="nota">Todo se cotiza desde cero.</p>
 							<a href="#" class="btn">Mas información</a>
 						</div>
 					</div>
@@ -210,7 +234,7 @@
 						</div>
 					</div>
 					<div class="col-md-4">
-						<img src="img/logosolo.png" alt="" style="align-content: right;">
+						<img class="img-fluid" src="img/logosolo.png" alt="" style="align-content: right;">
 					</div>
 				</div>
 			</div>
@@ -218,6 +242,9 @@
 	</footer>
 
 	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="js/bootstrap/bootstrap.min.js"></script>
+
 	<!--script type="text/javascript" src="js/fullpage/jquery.fullpage.min.js"></script>
 	<script type="text/javascript" src="js/fullpage/vendors/jquery.easings.min.js"></script>
 	<script type="text/javascript" src="js/fullpage/vendors/scrolloverflow.min.js"></script-->
