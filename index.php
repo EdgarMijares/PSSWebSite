0ba2a4aa7b5a9d83a64c8ea9de71bc64ed569f75
Edgar <progresssystemsoftware@gmail.com>
2018-01-18 01:20:45 -0600

    Terminar estilos

--- index.php
+++ index.php
@@ -168,7 +168,7 @@
 									<li>10 GB de espacio</li>
 									<li>Correo electronico</li>
 								</ul>
-								<p class="nota">Paquete basico no incluye mejoras.</p>
+								<p class="nota">Paquete básico no incluye mejoras de SEO pero si optimización de carga.</p>
 								<a href="#" class="btn">Mas información</a>
 							</div>
 						</div>
@@ -183,7 +183,7 @@
 									<li>10 GB de espacio</li>
 									<li>Base de datos</li>
 								</ul>
-								<p class="nota">No incluye el costo de la aplicaión.</p>
+								<p class="nota">El costo de la aplicación, se cotiza aparte según la complejidad de la misma.</p>
 								<a href="#" class="btn">Mas información</a>
 							</div>
 						</div>
@@ -198,7 +198,7 @@
 									<li>Desarrollo</li>
 									<li>Implementacón</li>
 								</ul>
-								<p class="nota">Todo se cotiza desde cero.</p>
+								<p class="nota">La cotización se hace en función y características del programa ajustable a presupuesto.</p>
 								<a href="#" class="btn">Mas información</a>
 							</div>
 						</div>
@@ -211,38 +211,86 @@
 		<div class="contacto">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-8">
-						<h3>Contacto</h3>
+					<div class="col-md-9">
+						<h3 class="titulo">Contacto</h3>
 						<div class="row">
-							<div class="col-md-12">
-								<button type="button" class="btn btn-primary">Formulario</button>
+							<div class="col-0 col-md-2"></div>
+							<div class="col-md-8">
+								<a href="#" class="btn">Formulario</a>
 							</div>
+							<div class="col-0 col-md-2"></div>
 						</div>
-						<div class="row">
-							<div class="col-4">
+						<div class="row redes">
+							<div class="col-2">
 								<a href="#" class="facebook"><span class="fa fa-facebook-official"></span></a>
 							</div>
-							<div class="col-4">
+							<div class="col-2">
 								<a href="#" class="linkedin"><span class="fa fa-linkedin"></span></a>
 							</div>
-							<div class="col-4">
+							<div class="col-2">
+								<a href="#" class="twitter"><span class="fa fa-twitter"></span></a>
+							</div>
+							<div class="col-2">
 								<a href="#" class="instagram"><span class="fa fa-instagram"></span></a>
 							</div>
+							<div class="col-2">
+								<a href="#" class="youtube"><span class="fa fa-youtube"></span></a>
+							</div>
+							<div class="col-2">
+								<a href="#" class="github"><span class="fa fa-github"></span></a>
+							</div>
 						</div>
-						<div class="row">
+						<div class="row contactos">
 							<div class="col-md-6">
 								<a href="tel:016181859879" class=""><span class="fa fa-phone-square"></span> 618-185-98-79</a>
 							</div>
 							<div class="col-md-6">
 								<a href="tel:016181571896" class=""><span class="fa fa-phone-square"></span> 618-157-18-96</a>
 							</div>
-							<div class="col-md-12">
+							<div class="col-md-12 email">
 								<a href="mailto:contacto@pssoftware.com.mx"><span class="fa fa-commenting-o"></span> CONTACTO@PSSOFTWARE.COM.MX</a>
 							</div>
 						</div>
 					</div>
-					<div class="col-md-4">
-						<img class="img-fluid" src="img/logosolo.png" alt="" style="align-content: right;">
+					<div class="col-md-3">
+						<img class="img-logo" src="img/logosolo.png" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-12">
+						<h3 class="titulo">Más sobre nosotros</h3>
+						<p>
+							Progress System Software trabaja desde el año 2014 en diferentes proyectos. Y contribuye de manera voluntaria en otro nuevos, si tienes un proyecto y no existe presupuesto puedes contactarnos y podremos ayudar a concretar tu idea y o sueño en realidad. Al igual trabajamos de la mano con diferentes empresas y organizaciones sociales para crear una comunidad mejor. Para mayor información de las áreas en las que trabajamos contáctanos en cualquiera de nuestras redes sociales o datos de contactos dentro de este mismo sitio.
+						</p>
+					</div>
+					<div class="col-md-4 col-12 comunidades">
+						<div class="row">
+							<div class="col-12">
+								<h3 class="titulo">Comunidades</h3>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-12">
+								<ul>
+									<li><a href="https://www.meetup.com/es-ES/GDG-InCode">GDG Incode</a></li>
+									<li><a href="https://www.facebook.com/HackerSpaceDGO/">InnovationRoom Hacker Space</a></li>
+									<li><a href="https://plus.google.com/u/1/communities/111865744656887712542?hl=es">Competencias Digitales</a></li>
+									<li><a href="https://plus.google.com/u/1/communities/107069834555915063534?hl=es">G Suite Referral Program</a></li>
+									<li><a href="http://bitcamp.com.mx/">Bit Lovers</a></li>
+									<li><a href="https://www.facebook.com/redbiot/">Red Biot</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-12">
+						<hr>
+					</div>
+					<div class="col-12">
+						Progress System Software Copyrights © 2018 All Rights
 					</div>
 				</div>
 			</div>
